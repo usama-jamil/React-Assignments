ce721ae3a96271d28d25de0cf9aa46b6b765e3ed
usamajamil006 <usamajamil006@gmail.com>
2019-01-24 19:10:29 +0500

    done thie assgnment

--- excercise-5-controlled-component/src/App.js
+++ excercise-5-controlled-component/src/App.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(prop) {
+    super(prop);
+    this.state = { txt: "" };
+    this.hanldeChange = this.hanldeChange.bind(this);
+  }
+
+  hanldeChange(e) {
+    console.log(e.currentTarget.className)
+    this.setState(
+      { [e.currentTarget.className]:e.currentTarget.value}
+    )
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Bootcamp - Train the Trainer - Coding Excercise</h1>        
+          <h1 className="App-title">
+            React Bootcamp - Train the Trainer - Coding Excercise
+          </h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Write your text here" />
+          <input
+            type="text"
+            className="txt"
+            value={this.state.txt}
+            onChange={this.hanldeChange}
+            placeholder="Write your text here"
+          />
           <p className="echo">You are saying:</p>
-          <p>What ever you type into the input field it should reflect here.</p>
+          <p>What ever you type into the input field it should reflect here. {this.state.txt}</p>
         </div>
       </div>
     );
@@ -21,4 +41,3 @@ class App extends Component {
 }
 
 export default App;
-
